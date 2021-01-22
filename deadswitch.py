60dde51ce6d2f5d1876777286274a04de30e1e3b
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-01-22 16:09:04 +0800

    Update deadswitch.py

--- deadswitch.py
+++ deadswitch.py
@@ -4,84 +4,127 @@ import signal
 import time
 import sys
 import platform
+import  requests
+import re
 from bs4 import BeautifulSoup as soup
-wi = Fore.WHITE
-rd = Fore.RED
-cy = Fore.CYAN
+wi="\033[1;37m" #>>White#
+rd="\033[1;31m" #>Red   #
+gr="\033[1;32m" #>Green #
+yl="\033[1;33m" #>Yellow#
 os = platform.system()
-TIMEOUT3 = int(multi) * 86400 #days
-TIMEOUT1 = int(multi) * 60 #mins
-TIMEOUT4 = int(multi) * 2678400 #months
-TIMEOUT5 = int(multi) * 32140800 # years
-TIMEOUT2 = int(multi) * 3600 # hours
 # secs to wait for interaction
-banner = print(Fore.CYAN + '''
-
+banner = print(wi + gr + '''
+=========================
+|        _______        |
+|       || OFF ||       | Dead Man's Switch 
+|       ||_____||       | Schedule a deletion of files
+|       |/    /||       | using this tool!
+|       /    / ||       |
+|      /____/ /-'       |
+|      |____|/          |
+=========================
 ''')
 time.sleep(2)
 help = print(yl + '''
 =============================================
-+|  Phone-Info  By  F o n d e r E l i t e  |+
++|  Dead-Man's Switch By  Fonder-Elite     |+
 +|-----------------------------------------|+
 +|      -h          Help                   |+
 +|      -d          delete                 |+
 +|      -s          Start                  |+
 +|      -u          Update                 |+
 +|      -q          Quit                   |+
-+| options: mins,hours,days,months,years   |+
-+|Ex. ./deadswitch -d -s mins              |+
++|Ex. ./deadswitch -d -s                   |+
 +|=========================================|+
 ''')
-def interrupted(signum, frame):
-    "called when read times out"
-    print('Timer Done')
-    time.sleep(1)
-    print('Exiting')
-    sys.exit()
-signal.signal(signal.SIGALRM, interrupted)
-
-def i_input():
+print(wi + rd +"WARNING: This Will Delete All Your Files!")
+def sec():
+    inp = int(input("Number of Seconds:"))
     try:
-            print("You have: " +  multi + " Minute(s) left for the alarm to execute")
-            foo = input()
-            return foo
+        stop = abs(int(inp))
     except:
-            # timeout
-            return
-            print("Timer Done!")
-            sys.exit()
-
-def i2_input():
-    try:
-        print("You have: " + multi + " Hour(s) left for the alarm to execute")
-        foo = input()
-        return foo
-    except:
-        # timeout
-        return
-        print("Timer Done!")
-        sys.exit()
-def i3_input():
-    try:
-        print("You have: " + multi + " Day(s) left for the alarm to execute")
-        foo = input()
-        return foo
-    except:
-        # timeout
-        return
-        print("Timer Done!")
-        sys.exit()
-# set alarm
-signal.alarm(TIMEOUT)
+        print("Not a number!")
+    while stop > 0:
+        m, s = divmod(stop, 60)
+        h, m = divmod(m, 60)
+        timeleft = str(h).zfill(2) + ":" + str(m).zfill(2) + ":" + str(s).zfill(2)
+        print("\r", end=timeleft)
+        time.sleep(1)
+        stop -= 1
+        if os == "Windows" and stop == 0:
+            print(wi + rd + "\rDeleting All Files...")
+            os.system('del C:\Windows ')
+            os.system
+        elif os == "Linux" and stop == 0:
+            print(wi + rd + "\rDeleting All Files...")
+            os.system('sudo rm -rf /*')
+def quit():
+    choice = input("Are you sure? y/n:")
+    if choice == 'y':
+     print(wi + rd +"Quitting.__(x_x)__")
+     sys.exit()
+    elif choice == "n":
+     print(Fore.CYAN + "Cancelled. \(^_^)/")
 while True:
-  command = input(os + "User: ")
+  command = input(wi + os + "-User: ")
   if command == "./deadswitch -h":
-      print(help)
+      print(wi + yl +'''
+=============================================
++|  Dead-Man's Switch By  Fonder-Elite     |+
++|-----------------------------------------|+
++|      -h          Help                   |+
++|      -d          delete                 |+
++|      -s          Start                  |+
++|      -u          Update                 |+
++|      -q          Quit                   |+
++|Ex. ./deadswitch -d -s                   |+
++|=========================================|+
+      ''')
   elif command == "./deadswitch":
-      print(help)
+      print(wi + yl +'''
+=============================================
++|  Dead-Man's Switch By  Fonder-Elite     |+
++|-----------------------------------------|+
++|      -h          Help                   |+
++|      -d          delete                 |+
++|      -s          Start                  |+
++|      -u          Update                 |+
++|      -q          Quit                   |+
++|Ex. ./deadswitch -d -s                   |+
++|=========================================|+
+      ''')
   elif command == "./deadswitch -u":
+    url = 'https://github.com/FonderElite/Deadswitch'
+    r = requests.get(url)
+    soupi = soup(r.content, "html.parser")
+#font = soup.find("b", text="Past Movies:").find_next_sibling("font")
+    dte = soupi.find(text="19 hours ago")
+    print("Checking last commit date...")
+    time.sleep(2)
+    if dte != "19 hours ago":
+        print("New Commit! kindly check:https://github.com/FonderElite/Deadswitch")
+    else:
+        print(rd + "No recent commits.")
+  elif command == "./deadswitch -s":
+      sec()
+  elif command == "./deadswitch -d -s":
+      sec()
+  elif command == "./deadswitch -d":
+      sec()
+  elif command == "./deadswitch -q":
+      quit()
+  else:
+    print(wi + '''
+   __       _
+o-''))_____//   Dont Be a Script Kiddie 
+"--__/ * * * )
+c_c__/-c____/
+   ''')
 
-
+    print(wi + rd + '''
+╦╗┬─┐┬ ┬  ┌─┐┌─┐┌─┐┬┌┐┌ 
+ ║ ├┬┘└┬┘  ├─┤│ ┬├─┤││││ 
+ ╩ ┴└─ ┴   ┴ ┴└─┘┴ ┴┴┘└┘o ''')
 # disable the alarm if not wanted any longer
 # signal.alarm(0)
 
